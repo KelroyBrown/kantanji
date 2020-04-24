9195d23c10eed8b2ae737981b3378c781ed22f56
Kelroy <wakatta@gmail.com>
2020-04-24 12:57:19 +0900

    Add files via upload

---
+++ kanjilist.py
@@ -0,0 +1,30 @@
+#kantanji list of kanji
+#hope to replace with a database in the future
+#Japanese Elementary School 1st Grade Kanji
+kanji1 = [('一', 'One', '1' , 'ひと, ひとつ' , 'イチ, イツ') ,
+          ('ニ', 'Two', '2' , 'ふた, ふたつ, ふたたび' , 'ニ, ジ') ,
+          ('三', 'Three', '3' , 'み, みつ, みっつ' , 'サン, ゾウ') ,
+          ('四', 'Four', '5' , 'よ, よつ, よっつ, よん' , 'シ') ,
+          ('五', 'Five', '4' , 'いつ, いつつ' , 'ゴ') ,
+          ('六', 'Six', '4' , 'む, むつ, むっつ, むい' , 'ロク, リク') ,
+          ('七', 'Seven', '2' , 'なな, ななつ, なの' , 'シチ') ,
+          ('八', 'Eight', '2' , 'や, やつ, やっつ, よう' , 'ハチ') ,
+          ('九', 'Nine', '2' , 'ここの, ここのつ' , 'キュウ, ク') ,
+          ('十', 'Ten', '2' , 'とお, と, そ' , 'ジュウ, ジッ, ジュッ')]
+
+# TODO: finish list
+
+
+
+
+# TODO:
+#Japanese Elementary School 2nd Grade Kanji
+kanji2 = []
+#Japanese Elementary School 3rd Grade Kanji
+kanji3 = []
+#Japanese Elementary School 4th Grade Kanji
+kanji4 = []
+#Japanese Elementary School 5th Grade Kanji
+kanji5 = []
+#Japanese Elementary School 6th Grade Kanji
+kanji6 = []
---
+++ kantanji.py
@@ -0,0 +1,23 @@
+#! python3
+# kantanji.py - Kanji readings and stoke counts from a list with data
+
+from kanjilist import *
+
+num = 0
+
+while num < len(kanji1):
+    print(kanji1[num][0] + ' means ' + kanji1[num][1] + ' and has ' + kanji1[num][2] + ' stroke(s).\n' + 'Kun reading(s) ' + kanji1[num][3] + '\nOn reading(s) ' + kanji1[num][4])
+    print('Learning Japanese? Free resources at Jamaipanese.com')
+    print('\n')
+    num += 1
+    if num == len(kanji1):
+        print ('all Kanji printed')
+
+
+##https://automatetheboringstuff.com/
+##https://automatetheboringstuff.com/2e/chapter9/ - quiz idea base
+##https://en.wikipedia.org/wiki/Ky%C5%8Diku_kanji#First_grade_(80_kanji)
+##http://www.kanji-a-day.com/
+##https://www.kanjireview.com/
+##https://www.japandict.com/
+##https://jisho.org/
